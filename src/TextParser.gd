eb09d1f956e915fd39b13710ac540ba616ad4fb3
7Havn <fishbart@gmail.com>
2024-01-18 00:38:04 +0000

    /dev/fd/63

--- src/TextParser.gd
+++ src/TextParser.gd
@@ -1,65 +1,62 @@
 class_name TextParser
 
-var InstructionSet = load("res://src/InstructionSet.gd")
-
+var InstructionS = load("res://src/InstructionSet.gd")
 var object = null
 
-# Parse a given input string into an instruction.
+# Parse input string to instruction.
 func parse(text):
-	match text:
-		'go north':
-			return InstructionSet.NORTH
-		'north':
-			return InstructionSet.NORTH
-		'go south':
-			return InstructionSet.SOUTH
-		'south':
-			return InstructionSet.SOUTH
-		'go east':
-			return InstructionSet.EAST
-		'east':
-			return InstructionSet.EAST
-		'go west':
-			return InstructionSet.WEST
-		'west':
-			return InstructionSet.WEST
+	# Convert the input text to lowercase
+	var lowerText = text.to_lower()
 
-		'look':
-			return InstructionSet.LOOK
-		'help':
-			return InstructionSet.HELP
-		'help me':
-			return InstructionSet.HELP
+	# Check commands - return instruction from InstructionSet.gd
+	if lowerText == 'start':
+		return InstructionS.START
+	if lowerText in ['go north', 'north']:
+		return InstructionS.NORTH
+	elif lowerText in ['go south', 'south']:
+		return InstructionS.SOUTH
+	elif lowerText in ['go east', 'east']:
+		return InstructionS.EAST
+	elif lowerText in ['go west', 'west']:
+		return InstructionS.WEST
 
-		'reset':
-			return InstructionSet.RESET
-		'quit':
-			return InstructionSet.QUIT
-		'exit':
-			return InstructionSet.QUIT
+	elif lowerText in ['look around', 'look']:
+		return InstructionS.LOOK
+	elif lowerText in ['help', 'help me']:
+		return InstructionS.HELP
 
-	if text.begins_with('get '):
+	elif lowerText == 'reset game':
+		return InstructionS.RESET
+	elif lowerText in ['quit', 'exit']:
+		return InstructionS.QUIT
+
+	# If "get" used then extract the object
+	if lowerText.begins_with('get '):
 		var regex = RegEx.new()
 		regex.compile("get\\s(?<object>.*(\\s.*)?)")
-		var results = regex.search(text)
+		var results = regex.search(text.to_lower())
 		object = results.get_string('object')
-		return InstructionSet.GET
+		return InstructionS.GET
 
-	if text.begins_with('open '):
+	# If "open" command then extract the object
+	if lowerText.begins_with('open '):
 		var regex = RegEx.new()
 		regex.compile("open\\s(?<object>.*(\\s.*)?)")
-		var results = regex.search(text)
+		var results = regex.search(text.to_lower())
 		object = results.get_string('object')
-		return InstructionSet.OPEN
+		return InstructionS.OPEN
 
-	if text.begins_with('close '):
+	# If "close" used then extract the object using
+	if lowerText.begins_with('close '):
 		var regex = RegEx.new()
 		regex.compile("close\\s(?<object>.*(\\s.*)?)")
-		var results = regex.search(text)
+		var results = regex.search(text.to_lower())
 		object = results.get_string('object')
-		return InstructionSet.CLOSE
+		return InstructionS.CLOSE
 
-	return InstructionSet.NOT_FOUND
+	# Return NOT_FOUND from InstructionSet if comment non-existent
+	return InstructionS.NOT_FOUND
 
+# Function to get the parsed object
 func get_object():
 	return object
